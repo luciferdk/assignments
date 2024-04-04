134eb976aae15aad1b380e2fb69c9b509e44db28
luciferdk <dksinha0701@gmail.com>
2024-04-04 17:29:58 +0530

    CountVowels-solutation

--- 01-js/medium/countVowels.js
+++ 01-js/medium/countVowels.js
@@ -6,7 +6,43 @@
 */
 
 function countVowels(str) {
-    // Your code here
+  // Your code here
+  
+  /*
+  
+  var count = 0;
+
+  for (const char of str.toLowerCase()) {
+    if (char === 'a' || char === 'e' || char === 'i' || char === 'o' || char === 'u') {
+      count++;
+    }
+
+  }
+  return count;
+ */
+
+
+  // ------------------------------------------------------------------------------------------- 
+
+  const vowels = ['a', 'e', 'i', 'o', 'u'];
+
+  // Initialize the count
+  let count = 0;
+
+  // Convert the string to lowercase
+  // str = str.toLowerCase();
+
+  // Iterate over each character in the string
+  for (let letter of str.toLowerCase()) {
+    if (vowels.includes(letter)) {
+      count++;
+    }
+  }
+
+  // Return the number of vowels
+  return count; 
+  /*
+*/
 }
 
 module.exports = countVowels;
