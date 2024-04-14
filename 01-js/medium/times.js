b6d17902e17e880b395c50bb9387bb3d363dd1c2
luciferdk <dksinha0701@gmail.com>
2024-04-14 13:03:41 +0530

    times probleam solutation

--- 01-js/medium/times.js
+++ 01-js/medium/times.js
@@ -8,6 +8,27 @@ Hint - use Date class exposed in JS
 There is no automated test for this one, this is more for you to understand time goes up as computation goes up
 */
 
-function calculateTime(n) {
-    return 0.01;
+function calculateExecutionTime(n) {
+    const startTime = new Date().getTime();
+
+    const sum = calculateSum(n);
+
+    const endTime = new Date().getTime();
+    const timeElapsed = (endTime - startTime) / 1000; 
+
+    console.log(`Total sum from 1 to ${n}: ${sum}`);  // Log the sum
+    console.log(`Time to calculate: ${timeElapsed} seconds`);
+
+    return timeElapsed; // You can still return the time if needed
+}
+
+function calculateSum(n) {
+    return n * (n + 1) / 2;
+}
+
+// Test cases
+const testValues = [100, 100000, 1000000000]; 
+
+for (const n of testValues) {
+    calculateExecutionTime(n); 
 }
